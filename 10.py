e0603046eda5e0b6a174c6ffcc084c6f5cd111bd
yjthay <thayyeejie@gmail.com>
2019-06-09 13:13:59 +0100

    Solution for 10. Regular Expression Matching

---
+++ 10.py
@@ -0,0 +1,29 @@
+'''
+10. Regular Expression Matching
+
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+'''
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        p = "^"+p+"$"
+        reg = re.compile(p)
+        if re.search(reg,s):
+            return True
+        else:
+            return False
